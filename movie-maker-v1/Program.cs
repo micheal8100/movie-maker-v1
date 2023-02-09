beff921134e6909b4eb0d7911615eaada9aaca09
micheal8100 <99847225+micheal8100@users.noreply.github.com>
2023-02-10 11:38:22 +1300

    Add project files.

---
+++ movie-maker-v1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace movie_maker_v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
